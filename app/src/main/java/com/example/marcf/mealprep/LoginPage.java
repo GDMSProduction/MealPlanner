133ab33d55eed8abeb1cb14eb9608f66e27a39a1
Marc Figueroa <MAFigueroa@student.fullsail.edu>
2018-10-04 16:35:25 -0400

    calendar tab layout

--- app/src/main/java/com/example/marcf/mealprep/LoginPage.java
+++ app/src/main/java/com/example/marcf/mealprep/LoginPage.java
@@ -5,20 +5,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
 
 public class LoginPage extends AppCompatActivity {
 
-
+private Button loggingInBut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
 
+        loggingInBut = (Button) findViewById(R.id.LoginPageBut);
+        loggingInBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                openCalenderTab();
+
+            }
+        });
 
     }
 
+      public void openCalenderTab(){
+
+        Intent intentz = new Intent(this, CalendarTab.class );
+        startActivity(intentz);
+
+
+      }
 
 
 }
--- app/src/main/java/com/example/marcf/mealprep/SignUpPage.java
+++ app/src/main/java/com/example/marcf/mealprep/SignUpPage.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 
 public class SignUpPage extends AppCompatActivity {
   private Button backbutton;
+  private Button toCalendarPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +22,23 @@ public class SignUpPage extends AppCompatActivity {
                 openStartApp();
             }
         });
+
+        toCalendarPage = (Button) findViewById(R.id.SignUpPageBut);
+        toCalendarPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OpenCalendarPage();
+            }
+        });
+
     }
+
+        public void OpenCalendarPage(){
+
+           Intent intent = new Intent(this, CalendarTab.class);
+           startActivity(intent);
+
+        }
 
         public void openStartApp()
         {
