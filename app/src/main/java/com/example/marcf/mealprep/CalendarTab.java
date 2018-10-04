58883f79624056a44c95be2567dbdddd696da93c
Marc Figueroa <MAFigueroa@student.fullsail.edu>
2018-10-04 18:15:45 -0400

    finished connecting info to sign out
    calendar to sign out
    
    info to calendar
    
    calendar to info

--- app/src/main/java/com/example/marcf/mealprep/CalendarTab.java
+++ app/src/main/java/com/example/marcf/mealprep/CalendarTab.java
@@ -1,13 +1,51 @@
 package com.example.marcf.mealprep;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CalendarTab extends AppCompatActivity {
+
+    private Button infoBut;
+
+    private Button signOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar_tab);
+
+        infoBut = (Button) findViewById(R.id.CalendarPageInflBut);
+        infoBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openInfoPage();
+            }
+        });
+
+       signOut = (Button) findViewById(R.id.CalendarSignOut);
+       signOut.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               openStartPagefromCalendar();
+           }
+       });
+
     }
+
+     public void openStartPagefromCalendar()
+     {
+         Intent intentzzz = new Intent(this, StartApp.class);
+         startActivity(intentzzz);
+     }
+
+    public void openInfoPage()
+    {
+        Intent intentsss = new Intent(this, InfoPage.class);
+        startActivity(intentsss);
+    }
+
+
 }
--- app/src/main/java/com/example/marcf/mealprep/InfoPage.java
+++ app/src/main/java/com/example/marcf/mealprep/InfoPage.java
@@ -1,22 +1,56 @@
 package com.example.marcf.mealprep;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class InfoPage extends AppCompatActivity {
 
-
+private Button calendar;
+private Button SignOutInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_page);
 
+        calendar = (Button) findViewById(R.id.infoPageCalendarBut);
+        calendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openCalendarPagefromInfo();
+
+            }
+        });
+
+        SignOutInfo = (Button) findViewById(R.id.InfoSignOut);
+        SignOutInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                InfotoStart();
+            }
+        });
+
 
     }
+
+    public void openCalendarPagefromInfo()
+    {
+        Intent intenz = new Intent(this, CalendarTab.class);
+        startActivity(intenz);
+    }
+
+    public void InfotoStart()
+    {
+        Intent intens = new Intent(this, StartApp.class);
+        startActivity(intens);
+    }
+
 
 }
