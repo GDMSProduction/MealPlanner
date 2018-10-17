9a80ccc8618145445c0f9f3abde3d08510581f69
junjiechen1 <37597962+junjiechen1@users.noreply.github.com>
2018-10-16 20:13:18 -0400

    calculator

--- app/src/main/java/com/example/junji/meal/ProfilePage1.java
+++ app/src/main/java/com/example/junji/meal/ProfilePage1.java
@@ -1,15 +1,33 @@
 package com.example.junji.meal;
 
 import android.content.Intent;
+import android.database.CursorJoiner;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.NumberKeyListener;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.text.NumberFormat;
 
 public class ProfilePage1 extends AppCompatActivity {
 
     private Button Calorie;
     private Button Home ;
+    private Button click;
+    private int result;
+   private TextView week1;
+    private TextView week2;
+    private TextView week3;
+    private TextView LastWeight;
+    public int x;
+    public int y;
+    public int z;
+    public int a;
+    public int b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +46,42 @@ public class ProfilePage1 extends AppCompatActivity {
                 OpenHomePage();
             }
         });
+        LastWeight = findViewById(R.id.WeightResult);
+
+        click = findViewById(R.id.Weightresultclick);
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Result();
+            }
+        });
+
+        week1 = findViewById(R.id.WeightWeek1);
+        week2 = findViewById(R.id.WeightWeek2);
+        week3 = findViewById(R.id.WeightWeek3);
+        if(week1.getText().toString().isEmpty()){
+            week1.setText("0");
+        }
+        if(week2.getText().toString().isEmpty()){
+            week2.setText("0");
+        }
+        if(week3.getText().toString().isEmpty()){
+            week3.setText("0");
+        }
+
+        x = Integer.parseInt( week1.getText().toString());
+         y = Integer.parseInt( week2.getText().toString());
+         z = Integer.parseInt( week3.getText().toString());
+
+
+             a = x - y;
+             b =  y - z;
+             result = a+b;
+
+             LastWeight.setText("Weight Lost: "+ result);
+
+
+
     }
     public void OpenCaloriePage(){
         Intent intent = new Intent(this,ProfilePage2.class);
@@ -38,4 +92,15 @@ public class ProfilePage1 extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void Result(){
+        x = Integer.parseInt( week1.getText().toString());
+        y = Integer.parseInt( week2.getText().toString());
+        z = Integer.parseInt( week3.getText().toString());
+
+        a = x - y;
+        b =  y - z;
+        result = a+b;
+
+        LastWeight.setText("Weight Lost: "+ result);
+    }
 }
--- app/src/main/java/com/example/junji/meal/ProfilePage2.java
+++ app/src/main/java/com/example/junji/meal/ProfilePage2.java
@@ -5,10 +5,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class ProfilePage2 extends AppCompatActivity {
     private Button Weight;
     private Button Home ;
+    private Button click;
+
+    private int result;
+    private TextView week1;
+    private TextView week2;
+    private TextView week3;
+    private TextView total;
+    public int cal1;
+    public  int cal2;
+    public int cal3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +39,37 @@ public class ProfilePage2 extends AppCompatActivity {
                 OpenHomePage();
             }
         });
+        click = findViewById(R.id.Weightresultclick2);
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Resultss();
+            }
+        });
+
+        total = findViewById(R.id.WeightResult);
+
+        week1 = findViewById(R.id.WeightWeek1);
+        week2 = findViewById(R.id.WeightWeek2);
+        week3 = findViewById(R.id.WeightWeek3);
+        if(week1.getText().toString().isEmpty()){
+            week1.setText("0");
+        }
+        if(week2.getText().toString().isEmpty()){
+            week2.setText("0");
+        }
+        if(week3.getText().toString().isEmpty()){
+            week3.setText("0");
+        }
+
+        cal1 = Integer.parseInt( week1.getText().toString());
+        cal2 = Integer.parseInt( week2.getText().toString());
+        cal3 = Integer.parseInt( week3.getText().toString());
+
+
+        result = cal1 + cal2 + cal3;
+
+        total.setText("Cal Consumed: "+ 0);
     }
     public void OpenCaloriePage(){
         Intent intent = new Intent(this,ProfilePage1.class);
@@ -36,4 +79,15 @@ public class ProfilePage2 extends AppCompatActivity {
         Intent intent = new Intent(this,HomePage.class);
         startActivity(intent);
     }
+    public void Resultss(){
+        cal1 = Integer.parseInt( week1.getText().toString());
+        cal2 = Integer.parseInt( week2.getText().toString());
+        cal3 = Integer.parseInt( week3.getText().toString());
+
+
+        result = cal1 + cal2 + cal3;
+
+        total.setText("Cal Consumed: "+ result);
+    }
+
 }
