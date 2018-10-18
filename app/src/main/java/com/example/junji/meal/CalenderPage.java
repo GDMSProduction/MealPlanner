fa89e9922699e682e53e4ddf6e24fdfe3cd98a86
junjiechen1 <37597962+junjiechen1@users.noreply.github.com>
2018-10-18 16:00:31 -0400

    vugjuhgyvjuhyvjuhyvjhv

--- app/src/main/java/com/example/junji/meal/CalenderPage.java
+++ app/src/main/java/com/example/junji/meal/CalenderPage.java
@@ -1,15 +1,16 @@
 package com.example.junji.meal;
-
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.CalendarView;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class CalenderPage extends AppCompatActivity {
 
@@ -17,8 +18,22 @@ public class CalenderPage extends AppCompatActivity {
     private Button home;
     private Button info;
     private Button recipe;
-    private Button edit;
+    private CalendarView mcalendarView;
 
+    public String Date;
+    public String StartDay = "0";
+    private TextView startdaytext;
+
+    private int Maxday;
+    private int beginningweek1;
+    private int beginningweek2;
+    private int beginningweek3;
+    private int endofweek1;
+    private int endofweek2;
+    private int endofweek3;
+    private int Dateday;
+    private int Datemonth;
+    private int Dateyear;
     public String week1 = "Breakfast: " +
             "\n-1 toast and 2 scrambled eggs" +
             "\n OR" +
@@ -77,13 +92,6 @@ public class CalenderPage extends AppCompatActivity {
         //TextView Textscroll = findViewById(R.id.Textscroll) ;
         //Textscroll.setMovementMethod(new ScrollingMovementMethod());
 
-        edit = (Button) findViewById(R.id.Edit);
-        edit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                OpenCalender();
-            }
-        });
 
         signout = (Button) findViewById(R.id.SignOutButton);
         signout.setOnClickListener(new View.OnClickListener() {
@@ -117,6 +125,55 @@ public class CalenderPage extends AppCompatActivity {
             }
         });
 
+        startdaytext = findViewById(R.id.Currentday);
+        mcalendarView = (CalendarView) findViewById(R.id.calendarView);
+        mcalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange( CalendarView calendarView, int year, int month, int dayOfMonth) {
+                Date = year+"/"+month+"/"+dayOfMonth;
+                if(StartDay == "0"){
+                    Dateyear = year;
+                    Datemonth = month;
+                    Dateday = dayOfMonth;
+                    StartDay =Date;
+                    Calendar calendar = Calendar.getInstance();
+                    Maxday = Calendar.DAY_OF_MONTH;
+                    beginningweek1= dayOfMonth;
+                    endofweek1 =(dayOfMonth + 6)%Maxday;
+                    beginningweek2 = (endofweek1 +1)%Maxday;
+                    endofweek2 = (beginningweek2 +6)%Maxday;
+                    beginningweek3 = (endofweek2 +1)%Maxday;
+                    endofweek3 = (beginningweek3 +6)%Maxday;
+
+                }
+
+                if(dayOfMonth >= beginningweek1 && dayOfMonth<= endofweek1){
+                    OpenWeek1();
+                }
+                if(dayOfMonth >= beginningweek2 && dayOfMonth<= endofweek2){
+                    OpenWeek2();
+                }
+                if(dayOfMonth >= beginningweek3 && dayOfMonth<= endofweek3){
+                    OpenWeek3();
+                }
+
+
+
+               // System.out.println("First Day Of Month : " + calendar.getActualMinimum(Calendar.DAY_OF_MONTH));
+               // System.out.println("Last Day of Month  : " + calendar.getActualMaximum(Calendar.DAY_OF_MONTH));
+
+              // startdaytext.setText("Last Day of Month  : " + calendar.getActualMaximum(Calendar.DAY_OF_MONTH));
+                //  startdaytext.setText("Start day:" + Date);
+
+               // if()
+            }
+        });
+
+        String date_d = new SimpleDateFormat("MMM dd, yyyy", Locale.getDefault()).format(new Date());
+
+
+        startdaytext.setText("Current day:"+date_d);
+
         //TextView calendarList = findViewById(R.id.Textscroll);
         //calendarList.setText(week1);
 
@@ -144,6 +201,19 @@ public class CalenderPage extends AppCompatActivity {
     }
     public void OpenCalender(){
         Intent intent = new Intent(this,Calenderdecision.class);
+        startActivity(intent);
+    }
+
+    public  void OpenWeek1(){
+        Intent intent = new Intent(this,Week1_Meal.class);
+        startActivity(intent);
+    }
+    public  void OpenWeek2(){
+        Intent intent = new Intent(this,Week2_Meal.class);
+        startActivity(intent);
+    }
+    public  void OpenWeek3(){
+        Intent intent = new Intent(this,Week3_Meal.class);
         startActivity(intent);
     }
 }
